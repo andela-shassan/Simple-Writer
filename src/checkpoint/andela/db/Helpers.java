431ed1785174d97164e6410c86176cf9c9a89a7a
andela-shassan <hassan.ayomon@andela.com>
2016-01-19 11:31:00 +0100

    [Finishes #111876127] Create the helper chore to help hold some data

---
+++ src/checkpoint/andela/db/Helpers.java
@@ -0,0 +1,62 @@
+package checkpoint.andela.db;
+
+public  class Helpers {
+  
+  static final String DRIVER = "com.mysql.jdbc.Driver";  
+  static final String DB_URL = "jdbc:mysql://localhost:3306/";
+
+  static final String USER = "root";
+  static final String PASS = "Nobest@86";
+  
+  public Helpers() {
+    
+  }
+  
+  
+  public static String[] attribute = {
+      "UNIQUE-ID",
+      "TYPES",
+      "COMMON-NAME",
+      "ATOM-MAPPINGS",
+      "CANNOT-BALANCE?",
+      "CITATIONS",
+      "COMMENT",
+      "COMMENT-INTERNAL",
+      "CREDITS",
+      "DATA-SOURCE",
+      "DBLINKS",
+      "DELTAG0",
+      "DOCUMENTATION",
+      "EC-NUMBER",
+      "ENZYMATIC-REACTION",
+      "ENZYMES-NOT-USED",
+      "EQUILIBRIUM-CONSTANT",
+      "HIDE-SLOT?",
+      "IN-PATHWAY",
+      "INSTANCE-NAME-TEMPLATE",
+      "LEFT",
+      "MEMBER-SORT-FN",
+      "ORPHAN?",
+      "PATHOLOGIC-NAME-MATCHER-EVIDENCE",
+      "PATHOLOGIC-PWY-EVIDENCE",
+      "PHYSIOLOGICALLY-RELEVANT?",
+      "PREDECESSORS",
+      "PRIMARIES",
+      "REACTION-DIRECTION",
+      "REACTION-LIST",
+      "REGULATED-BY",
+      "REQUIREMENTS",
+      "RIGHT",
+      "RXN-LOCATIONS",
+      "SIGNAL",
+      "SPECIES",
+      "SPONTANEOUS?",
+      "STD-REDUCTION-POTENTIAL",
+      "SYNONYMS",
+      "SYSTEMATIC-NAME",
+      "TEMPLATE-FILE",
+      "^COEFFICIENT",
+      "^COMPARTMENT"
+  };
+  
+}
